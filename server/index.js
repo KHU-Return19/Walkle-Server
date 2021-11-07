49066bd87128db489549cc8f89ca8284386bbd28
seoh02h <seok02h@khu.ac.kr>
2021-11-08 02:32:50 +0900

    Add community router

--- server/index.js
+++ server/index.js
@@ -1,36 +1,33 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const app = express();
 
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-require('dotenv').config();
-const port=process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
+const config = require("./config/key");
 
-const bodyParser=require('body-parser');
-const cookieParser=require('cookie-parser');
-
-const config=require('./config/key');
-
-const mongoose=require('mongoose');
-const connect = mongoose.connect(config.mongoURI,{
-    useNewUrlParser: true, useUnifiedTopology: true,
+const mongoose = require("mongoose");
+const connect = mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     // useCreateIndex: true, useFindAndModify: false
-    })
-    .then(()=>console.log('디비연결 성공'))
-    .catch((err)=>console.log(err));
-
+  })
+  .then(() => console.log("디비연결 성공"))
+  .catch((err) => console.log(err));
 
 // Routes
 
-
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users',require('./routes/users'));
-
-app.listen(port,()=>{
-    console.log(`Server Listening ${port}`)
-})
+app.use("/api/users", require("./routes/users"));
+app.use("/api/communities", require("./routes/communities"));
+app.listen(port, () => {
+  console.log(`Server Listening ${port}`);
+});
