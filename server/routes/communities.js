be772658d750a0de37768968d4fb955e31b6f544
hyun <seok02h@khu.ac.kr>
2021-11-11 04:00:52 +0900

    Add community routes

--- server/routes/communities.js
+++ server/routes/communities.js
@@ -5,25 +5,41 @@ const { Board } = require("../models/Community/Board");
 const { auth } = require("../middleware/auth");
 
 router.get("/", auth, (req, res) => {
-  Board.find((err, boards) => {
-    if (err) {
-      return res.json({ success: false, msg: err });
-    } else {
-      return res.json({ success: true, boards: boards });
-    }
-  });
+  let myboard = req.query.myboard;
+  if (myboard) {
+    let userId = req.user._id;
+    Board.find({ userId: userId }, (err, boards) => {
+      if (err) {
+        return res.json({ success: false, msg: err });
+      } else {
+        return res.json({ success: true, boards: boards });
+      }
+    });
+  } else {
+    Board.find((err, boards) => {
+      if (err) {
+        return res.json({ success: false, msg: err });
+      } else {
+        return res.json({ success: true, boards: boards });
+      }
+    });
+  }
 });
 
 router.get("/:no", auth, (req, res) => {
-  let userId = req.user._id;
-
   Board.findOne({ no: req.params.no }, (err, board) => {
     if (err) {
       return res.json({ success: false, msg: err });
     } else {
-      board.updateView(() => {
-        return res.json({ success: true, board: board });
-      });
+      if (!board) {
+        return res
+          .status(404)
+          .json({ succress: false, msg: "Board Not Found" });
+      } else {
+        board.updateView(() => {
+          return res.json({ success: true, board: board });
+        });
+      }
     }
   });
 });
@@ -53,25 +69,55 @@ router.put("/heart/:no", auth, (req, res) => {
     if (err) {
       return res.json({ success: false, msg: err });
     } else {
-      board.updateHeart(() => {
-        return res.json({ success: true, board: board });
-      });
+      if (!board) {
+        return res
+          .status(404)
+          .json({ succress: false, msg: "Board Not Found" });
+      } else {
+        board.updateHeart(() => {
+          return res.json({ success: true, board: board });
+        });
+      }
     }
   });
 });
 
-router.delete("/:no", auth, (req, res) => {
+router.put("/:no", auth, (req, res) => {
   let userId = req.user._id;
-  Board.findOneAndDelete(
+  Board.findOneAndUpdate(
     { no: req.params.no, userId: userId },
+    { title: req.body.title, content: req.body.content, updateAt: Date.now() },
     (err, board) => {
       if (err) {
         return res.json({ success: false, msg: err });
       } else {
-        return res.json({ success: true, board: board });
+        if (!board) {
+          return res
+            .status(404)
+            .json({ succress: false, msg: "No Match Board" });
+        } else {
+          return res.json({ success: true });
+        }
       }
     }
   );
+});
+
+router.delete("/:no", auth, (req, res) => {
+  let userId = req.user._id;
+  Board.findOne({ no: req.params.no, userId: userId }, (err, board) => {
+    if (err) {
+      return res.json({ success: false, msg: err });
+    } else {
+      if (!board) {
+        return res
+          .status(404)
+          .json({ succress: false, msg: "Board Not Found" });
+      } else {
+        return res.json({ success: true, board: board });
+      }
+    }
+  });
 });
 
 module.exports = router;
