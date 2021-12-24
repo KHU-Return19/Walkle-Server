2d4fbfd405114732f64ae411f32bb6b8825f7071
dogsoft0937 <dogsoft0937@khu.ac.kr>
2021-12-24 15:02:43 +0900

    Modify image upload function

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -22,18 +22,12 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage })
 
 router.post('/image', upload.single("file"), (req, res) => {
-    const { fieldname, originalname, encoding, mimetype, destination, filename, path, size } = req.file
-    const { name } = req.body;
-    console.log("body 데이터 : ", name);
-    console.log("폼에 정의된 필드명 : ", fieldname);
+    const { originalname, destination, filename, path, size } = req.file
     console.log("사용자가 업로드한 파일 명 : ", originalname);
-    console.log("파일의 엔코딩 타입 : ", encoding);
-    console.log("파일의 Mime 타입 : ", mimetype);
-    console.log("파일이 저장된 폴더 : ", destination);
     console.log("destinatin에 저장된 파일 명 : ", filename);
     console.log("업로드된 파일의 전체 경로 ", path);
     console.log("파일의 바이트(byte 사이즈)", size);
-    res.json({ ok: true, data: "Single Upload Ok" })
+    res.json({ ok: true, data: "Single Upload Ok",filename })
 })
 router.delete('/image/:imageId',(req,res)=>{
     fs.unlink(`uploads/${req.params.imageId}`,(err)=>{
